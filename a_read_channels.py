9090e4551b02cb5a6d62c887467b099e45053087
kelly-ning-shen <guozilideguozi@163.com>
2022-11-13 16:16:05 +0800

    record setting(12): chc(1), chp(11)

--- a_read_channels.py
+++ a_read_channels.py
@@ -7,7 +7,7 @@ import pyedflib
 import pandas as pd
 import os
 
-base = 'G:/NSRR/mnc/cnc/chp/'
+base = 'G:/NSRR/mnc/cnc/chc/'
 
 def findAllFile(base):
     for filepath, dirnames, filenames in os.walk(base):
@@ -19,6 +19,7 @@ def readSignalInfo(base,save=True):
     total = 25
     chs_names_pad = []
     subject = []
+    uniq_settings = []
     for filepath,filename in findAllFile(base):
         fullname = filepath + filename # filepath: G:/NSRR/mnc/cnc/chc/   filename: chc056-nsrr.edf
         print('\ndata path: ' + fullname)
@@ -35,9 +36,14 @@ def readSignalInfo(base,save=True):
 
             # concat 2 list with ': '
             for i in range(n):
-                signal_labels[i] += ': ' + str(signal_fs[i])
+                signal_labels[i] += ': ' + str(signal_fs[i]) # 'E1: 200'
             # print(signal_labels)
 
+            # find unique record settings
+            if signal_labels not in uniq_settings:
+                uniq_settings.append(signal_labels)
+
+            # add up
             ch_names_pad = signal_labels + [' ']*(total-len(signal_labels)) # total: 要取最大的通道数，这样才能形成一个矩阵
             chs_names_pad.append(ch_names_pad)
             subject.append(filename.split('.')[0]) # w/o .edf
@@ -50,17 +56,19 @@ def readSignalInfo(base,save=True):
     chs_names_dt = pd.DataFrame(chs_names_dict)
     print(chs_names_dt)
 
+    print('\nUnique settings: ', len(uniq_settings))
+    print(uniq_settings)
 
     if save:
         savetoExcel(chs_names_dt)
 
 def savetoExcel(chs_names_dt):
     # save dataframe to excel
-    writer = pd.ExcelWriter('log/channel/cnc_chp.xlsx') # pylint: disable=abstract-class-instantiated
+    writer = pd.ExcelWriter('log/channel/cnc_chc.xlsx') # pylint: disable=abstract-class-instantiated
     chs_names_dt.to_excel(writer)
     writer.save()
     print('DataFrame is written successfully to the Excel File.')
 
 
 if __name__ == '__main__':
-    readSignalInfo(base,save=True)
+    readSignalInfo(base,save=False)

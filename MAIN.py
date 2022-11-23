f78a49be25e1fb367448d281366604295339b2b0
kelly-ning-shen <guozilideguozi@163.com>
2022-11-23 15:13:46 +0800

    add Logger

--- MAIN.py
+++ MAIN.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,6 +9,8 @@ from config import AppConfig
 from diagnosis import Diagnose
 from a_tools import myprint
 
+savelog = 1
+
 DEBUG_MODE = False
 STANDARD_EPOCH_SEC = 30
 DEFAULT_SECONDS_PER_EPOCH = 30
@@ -15,6 +18,24 @@ DEFAULT_MINUTES_PER_EPOCH = 0.5  # 30/60 or DEFAULT_SECONDS_PER_EPOCH/60;
 
 DIAGNOSIS = ["Other","Narcolepsy type 1"]
 base = 'G:/NSRR/mnc/cnc/test/'
+
+if savelog:
+    class Logger(object):
+        def __init__(self, filename="Default.log"):
+            self.terminal = sys.stdout
+            self.log = open(filename, "a")
+    
+        def write(self, message):
+            self.terminal.write(message)
+            self.log.write(message)
+
+        def flush(self):
+            pass
+    
+    path = os.path.abspath(os.path.dirname(__file__))
+    type = sys.getfilesystemencoding()
+    # sys.stdout = Logger('log/withoutIH_AASM_right_IIRFil0.3_' + feature_type + '_nol.txt') # 不需要自己先新建txt文档  # right: filter_right
+    sys.stdout = Logger('log/preprpcess_test1.txt') # 不需要自己先新建txt文档  # right: filter_right
 
 def findAllFile(base):
     for filepath, dirnames, filenames in os.walk(base):

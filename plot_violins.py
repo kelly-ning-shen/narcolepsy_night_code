e1bb5c36cf1710cafd74fd47113934ca908b46d6
kelly-ning-shen <guozilideguozi@163.com>
2023-02-26 14:40:22 +0800

    plot

--- plot_violins.py
+++ plot_violins.py
@@ -2,19 +2,24 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-sns.set_theme(style="whitegrid")
+my_colors = ['#3494BA', '#FF9999']
+# sns.set_palette(my_colors)
+
+sns.set_theme(style="ticks")
 
 # Load the example tips dataset
-df = pd.read_excel('diagnosis\modelcomparision_2.5min.xlsx')
+df = pd.read_excel('diagnosis\modelcomparision_2.5min_multicnnc2cm.xlsx')
 
 # Draw a nested violinplot and split the violins for easier comparison
-fig, ax = plt.subplots()
-fig = sns.violinplot(data=df, x="participant", y="segment", hue="model",
-               split=True, inner="quart", linewidth=0.5,edgealpha=0.5,scale='count',cut=0,
-               palette={"Multitask": "b", "One-Phase": ".85"})
+fig, ax = plt.subplots(figsize=(20,4))
+fig = sns.boxplot(x='Participant', y='Likelihood of NT1 on 2.5 min segment', data=df, hue='Diagnosis', 
+                    flierprops={'marker':'+', 'color':'gray', 'alpha': 0.3}, palette=my_colors)
+# fig = sns.violinplot(data=df, x="participant", y="segment", hue="model",
+#                split=True, inner="quart", linewidth=0.5,edgealpha=0.5,scale='count',cut=0,
+#                palette={"Multitask": "b", "One-Phase": ".85"})
 sns.despine(left=True)
 plt.xticks(rotation = 90)
-
+plt.tight_layout()
 plt.show()
 plot = fig.get_figure()
-plot.savefig('test2_1.png',dpi=400) # save!
+plot.savefig('test3_3.png',dpi=220) # save!
